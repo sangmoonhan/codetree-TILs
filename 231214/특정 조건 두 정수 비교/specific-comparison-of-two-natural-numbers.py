2bee25da74b2d0b1644550011d2533dffcea4d7a
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-14 13:34:52 +0900

    Title: 특정 조건 두 정수 비교, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231214/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,16 @@
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+if a < b :
+    if a == b :
+        print(1,1)
+    else :
+        print(1,0)
+
+else :
+    if a == b :
+        print(0,1)
+    else :
+        print(0,0)

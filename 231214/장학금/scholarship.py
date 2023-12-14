7662240306a106bdec206803adfbdd4ed36cd025
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-14 23:16:27 +0900

    Title: 장학금, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231214/장학금/scholarship.py
@@ -0,0 +1,14 @@
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+if a >= 90 :
+    if b >= 95 :
+        print(10)
+    elif b >= 90 :
+        print(5)
+    else :
+        print(0)
+else :
+    print(0)

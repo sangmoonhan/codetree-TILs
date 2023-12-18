8ecb68ae0608a0e295e2d978a6b849364cce2d76
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-18 18:14:43 +0900

    Title: a 부터 b 까지, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231218/a 부터 b 까지/a-to-b.py
+++ 231218/a 부터 b 까지/a-to-b.py
@@ -2,10 +2,13 @@ arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 
-while a < b :
-    if a % 2 == 1 :
+while a <= b :
+    if a == b :
+        print(a, end=" ")
+        a +=1
+    elif a % 2 == 1 :
         print(a,end=" ")
         a *= 2
-    if a % 2 == 0 :
+    elif a % 2 == 0 :
         print(a,end=" ")
         a += 3

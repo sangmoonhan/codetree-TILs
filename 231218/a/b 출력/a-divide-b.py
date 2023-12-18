afe2d72898e3df78848a08595c885771704cb969
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-18 17:26:46 +0900

    Title: a/b 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231218/a/b 출력/a-divide-b.py
+++ 231218/a/b 출력/a-divide-b.py
@@ -1,11 +1,14 @@
-arr = input().split()
+# 정수 부분을 먼저 출력합니다.
+print(f"{a//b}.", end="")
 
-a = 10 * int(arr[0])
-b = int(arr[1])
+# a를 b로 나눈 나머지를 시작으로
+# 소수점 자리를 하나씩 계산합니다.
+a %= b
+for _ in range(20):
+    # 나머지에 10 곱한 값을 기준으로
+    # b로 나누었을 떄의 몫을 구해줍니다.
+    a *= 10
+    print(a // b, end="")
 
-#print(a/b)
-print("0.",end="")
-for i in range(0,20):
-    c = a//b
-    print(c,end="")
-    a = 10 *(a%b)
+    # a를 b로 나눈 나머지를 게속 갱신해줍니다.
+    a %= b

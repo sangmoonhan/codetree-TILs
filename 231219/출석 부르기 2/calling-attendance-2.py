43469f5f8aeed4a731a68ba541c6666fd82931aa
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-19 23:26:59 +0900

    Title: 출석 부르기 2, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231219/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+while True:
+    n = input()
+    n = int(n)
+    if n ==1 :
+        print("John")
+    elif n == 2 :
+        print("Tom")
+    elif n == 3 :
+        print("Paul")
+    elif n == 4 :
+        print("Sam")
+    else :
+        print("Vacancy")
+        break

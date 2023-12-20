07f86ed7597aebe59babff23e2777a46a5995b89
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-20 20:29:43 +0900

    Title: ab 사이에 없는 c, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231220/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,10 @@
+arr = input().split()
+a, b, c = int(arr[0]), int(arr[1]), int(arr[2])
+
+satisfied = "YES"
+for i in range(a, b + 1):
+    if i % c == 0:
+        satisfied = "NO"
+
+
+print(satisfied)

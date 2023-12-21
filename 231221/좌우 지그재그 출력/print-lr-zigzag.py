b3e60aa29d66efbdf5e217000e28315c848e16f3
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-21 20:40:14 +0900

    Title: 좌우 지그재그 출력, Time: 137ms, Memory: 26MB, Status: Passed - CodeTree

--- 231221/좌우 지그재그 출력/print-lr-zigzag.py
+++ 231221/좌우 지그재그 출력/print-lr-zigzag.py
@@ -1,3 +1,17 @@
+'''
+# 변수 선언 및 입력
+n = int(input())
+	
+# 좌우 지그재그로 출력합니다.
+for i in range(n):
+	for j in range(n):
+		if i % 2 == 0:
+			print((i * n) + j + 1, end=" ")
+		else:
+			print((i * n) + n - j, end=" ")
+	print()
+'''
+
 n = int(input())
 
 cnt =0

d3817af969a9ded961bd2dc90a3c7952e2877082
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-22 22:44:33 +0900

    Title: 특이한 출력, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231222/특이한 출력/special-print.py
+++ 231222/특이한 출력/special-print.py
@@ -1,7 +1,7 @@
 n = int(input())
 
-for i in range(1, 4): 
-    for j in range(1, 4):
+for i in range(1, n+1): 
+    for j in range(1, n+1):
         if (i + j) % 4 == 0:
             print(f"({i}, {j})", end="\n")
         else:

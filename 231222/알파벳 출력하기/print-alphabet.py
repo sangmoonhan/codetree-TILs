38aaea46b0aa777732d5a81f626bae1c5fb384b8
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-22 23:16:19 +0900

    Title: 알파벳 출력하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231222/알파벳 출력하기/print-alphabet.py
+++ 231222/알파벳 출력하기/print-alphabet.py
@@ -1,7 +1,11 @@
+''' A는 65 Z'''
 n = int(input())
 ch=65
 for i in range(1,n+1):
     for j in range(i):
         print(chr(ch),end="")
-        ch += 1
+        if ch == 90 :
+            ch = 65
+        else:
+            ch += 1
     print()

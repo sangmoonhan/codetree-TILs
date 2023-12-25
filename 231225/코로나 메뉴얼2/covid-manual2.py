139170cf3d693ec8e143e5325ba395a3f52f8f7d
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-25 19:39:11 +0900

    Title: 코로나 메뉴얼2, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231225/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,22 @@
+cnt = [0]*4
+
+for i in range(3):
+    arr = input().split()
+
+    a = arr[0]
+    b = int(arr[1])
+
+    if a == "Y" and b >= 37:
+        cnt[0] += 1
+    elif a == "N" and b >= 37:
+        cnt[1] += 1
+    elif a == "Y" and b < 37 :
+        cnt[2] += 1
+    else:
+        cnt[3] += 1
+
+for i in cnt:
+    print(i,end=" ")
+
+if cnt[0] >= 2 :
+    print("E")

2e658fc574bfc3fa003cbbecf6a1a2c6eb8d72bf
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-27 08:04:58 +0900

    Title: 공백을 기준으로 출력, Time: 231ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231227/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,20 @@
+arr = input()
+
+ch = []
+
+for i in arr :
+    if i ==" " :
+        continue
+    else :
+        ch.append(i)
+
+arr = input()
+
+for i in arr :
+    if i == " " :
+        continue
+    else :
+        ch.append(i)
+
+for i in ch:
+    print(i,end="")

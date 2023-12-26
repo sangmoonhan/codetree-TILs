a96b6d6aac2e5d569ee724695e81da557ce3fd64
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-26 14:20:37 +0900

    Title: 대문자로 바꾸기, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 231226/대문자로 바꾸기/change-to-capital.py
+++ 231226/대문자로 바꾸기/change-to-capital.py
@@ -1,6 +1,19 @@
-for i in range(5):
-    arr = input().split()
+# 2차원 배열을 구현해 각 줄마다 알파벳 소문자를 입력받습니다.
+arr_2d = [
+	list(input().split())
+	for _ in range(5)
+]
 
-    for j in range(3):
-        print(arr[j].upper(), end=" ")
-    print()
+# 알파벳 소문자를 대문자로 바꾸어 출력합니다.
+for i in range(5):
+	for j in range(3):
+		arr_2d[i][j] = chr(ord(arr_2d[i][j]) + ord('A') - ord('a'))
+		print(arr_2d[i][j], end=" ")
+	print()
+
+#for i in range(5):
+#    arr = input().split()
+#
+#    for j in range(3):
+#        print(arr[j].upper(), end=" ")
+#    print()

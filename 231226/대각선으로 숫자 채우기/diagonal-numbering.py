b25a6f0a0385943d543dd7099bb979de839310f7
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-26 15:36:59 +0900

    Title: 대각선으로 숫자 채우기, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231226/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -0,0 +1,20 @@
+arr = list(map(int,input().split()))
+
+n, m = arr[0], arr[1]
+
+max_val = 100
+
+arr = [ [0 for _ in range(m)] for _ in range(n) ]
+
+num = 1
+
+for k in range(max_val):
+    for l in range(max_val) :
+        if 0 <= l <= n-1 and 0 <= k-l <= m-1 : 
+            arr[l][k-l] = num
+            num+=1
+
+for i in arr:
+    for j in i :
+        print(j,end=" ")
+    print()

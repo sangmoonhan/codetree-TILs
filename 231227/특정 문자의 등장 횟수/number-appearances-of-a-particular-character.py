646afb3137849ed79030cc4ba7920a4f7ebc0d4f
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2023-12-27 14:36:41 +0900

    Title: 특정 문자의 등장 횟수, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 231227/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 231227/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,3 +1,12 @@
 ch = input()
 
-print(ch.count('ee'), ch.count('eb'))
+cnt1 = 0
+cnt2 = 0
+
+for i in range(0,len(ch)-1):
+    if ch[i:i+2] == 'ee' :
+        cnt1 += 1
+    if ch[i:i+2] == 'eb' :
+        cnt2 += 1
+
+print(cnt1, cnt2)

9adcd455a0b568a21b09870e9fe5e954456b1f24
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-03 10:59:26 +0900

    Title: 1이 되는 순간까지, Time: 239ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240103/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,14 @@
+def f(cnt,n):
+    if n == 1:
+        return cnt
+
+    if n % 2 == 0:
+        cnt += 1
+        return f(cnt ,n // 2)
+    else:
+        cnt += 1
+        return f(cnt, n // 3)
+
+n = int(input())
+
+print(f(0,n))

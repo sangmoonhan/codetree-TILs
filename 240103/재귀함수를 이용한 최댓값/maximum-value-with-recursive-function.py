09a437dcb274d84d45ecb1c9e8c46d442fc39242
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-03 11:41:56 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240103/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240103/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,16 +1,14 @@
-def f(n, arr):
-    
-    if n == 1:
-        if arr[0] > arr[1]:
-            return arr[0]
-        else :
-            return arr[1]
-    
-    if arr[n-1] > f(n-1,arr) : return arr[n-1]
-    else : return f(n-1,arr)
-
+# 변수 선언 및 입력:
 n = int(input())
+arr = list(map(int, input().split()))
 
-arr = list(map(int,input().split()))
 
-print(max(arr))
+# a번째 까지 인덱스의 숫자 중에 가장 큰 값을 반환합니다.
+def max_value(a):
+    if a == 0:
+        return arr[0]
+
+    return max(max_value(a - 1), arr[a])
+
+
+print(max_value(n - 1))

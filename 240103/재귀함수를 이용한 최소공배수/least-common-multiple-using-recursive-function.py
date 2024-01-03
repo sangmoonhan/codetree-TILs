bda32d5c2f42b4a677b36c300ec58262e6e6fe15
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-03 12:55:02 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 176ms, Memory: 24MB, Status: Passed - CodeTree

--- 240103/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240103/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -3,21 +3,17 @@ n = int(input())
 arr = list(map(int,input().split()))
 
 def f(n,arr) :
-    if n == 1 :
-        gcd = 1
-        for i in range(1, min(arr[0],arr[1]) + 1):
-            if arr[0] % i == 0 and arr[1] % i == 0 :
-                gcd=i
+    if n == 0 :
+        return arr[0]
 
-        return arr[0] * arr[1] // gcd
-
+    c = f(n-1,arr)
     gcd1 = 1
     
-    for j in range(1, min(f(n-1,arr),arr[n-1]) + 1):
+    for j in range(1, min(c,arr[n-1]) + 1):
         
-        if f(n-1,arr) % j == 0 and arr[n-1] % j == 0 :
+        if c % j == 0 and arr[n-1] % j == 0 :
             gcd1=j
 
-    return f(n-1,arr) * arr[n-1] // gcd1
+    return c * arr[n-1] // gcd1
 
 print(f(n,arr))

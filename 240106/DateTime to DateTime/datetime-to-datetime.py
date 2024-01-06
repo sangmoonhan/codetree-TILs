5f28310c5f02714dce029594b09fa9f45afaafe4
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-06 17:42:50 +0900

    Title: DateTime to DateTime, Time: 5087ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240106/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,27 @@
+a,b,c = tuple(map(int,input().split()))
+
+d = 11
+h = 11
+m = 11
+
+elapsed_mins = 0
+
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+while True:
+    if d == a and h == b and m == c:
+        break
+
+    elapsed_mins += 1
+    m += 1
+
+    if m == 60 :
+        h += 1
+        m = 0
+    
+    if h == 24 :
+        d += 1
+        h = 0
+
+print(elapsed_mins)

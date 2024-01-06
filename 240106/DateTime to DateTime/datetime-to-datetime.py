033a7a13d1c7bd0e11c4f6a55530298b48e029aa
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-06 18:13:13 +0900

    Title: DateTime to DateTime, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240106/DateTime to DateTime/datetime-to-datetime.py
+++ 240106/DateTime to DateTime/datetime-to-datetime.py
@@ -1,7 +1,36 @@
 a,b,c = tuple(map(int,input().split()))
 
-d =  (a - 12) * 24 * 60
-h = (24 + b - 12) * 60
-m = 60 + c - 11
+d = 11
+h = 11
+m = 11
 
-print(d + h + m)
+elapsed_mins = 0
+
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+if a < 11 :
+    print(-1)
+elif a == 11 :
+    if h < 11 :
+        print(-1)
+    elif h == 11 :
+        if m <= 11:
+            print(-1)
+else :
+    while True:
+        if d == a and h == b and m == c:
+            break
+
+        elapsed_mins += 1
+        m += 1
+
+        if m == 60 :
+            h += 1
+            m = 0
+    
+        if h == 24 :
+            d += 1
+            h = 0
+            
+    print(elapsed_mins)

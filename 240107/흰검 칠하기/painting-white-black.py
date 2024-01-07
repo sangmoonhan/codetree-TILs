f4b21d17e285a949e39a300c65fce3624d4380e1
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-07 18:17:52 +0900

    Title: 흰검 칠하기, Time: 206ms, Memory: 30MB, Status: Passed - CodeTree

--- 240107/흰검 칠하기/painting-white-black.py
+++ 240107/흰검 칠하기/painting-white-black.py
@@ -6,7 +6,8 @@ white = [0] * 200001
 w,b,g = 0,0,0
 
 start = 100000
-for _ in range(n):
+for j in range(n):
+
     cnt, direction = input().split()
     cnt = int(cnt)
 
@@ -14,13 +15,13 @@ for _ in range(n):
         for i in range(start,start+cnt):
             curr[i] = 1
             black[i] += 1 
-        start += cnt
+        start += (cnt-1)
     elif direction == "L":
-        for i in range(start-cnt, start):
+        for i in range(start-cnt+1, start+1):
             curr[i] = 2
             white[i] += 1
-        start -= cnt
-
+        start -= (cnt-1)
+    
 for i in range(200001):
     if white[i] >= 2 and black[i] >= 2:
         g += 1

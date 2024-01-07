447d85861e6935fa99aa420b82ab5b5811faf381
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-07 13:29:35 +0900

    Title: 최대로 겹치는 구간, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240107/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240107/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,18 +1,24 @@
+OFFSET = 100
+MAX_R = 200
+
+# 변수 선언 및 입력
 n = int(input())
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+checked = [0] * (MAX_R + 1)
 
-lines = []
-for _ in range(n):
-    x1, x2 = map(int, input().split())
-    lines.append((x1, x2))
+for x1, x2 in segments:
+    # OFFSET을 더해줍니다.
+    x1, x2 = x1 + OFFSET, x2 + OFFSET
+    
+    # 구간을 칠해줍니다.
+    # 구간 단위로 진행하는 문제이므로
+    # x2에 등호가 들어가지 않음에 유의합니다.
+    for i in range(x1, x2):
+        checked[i] += 1
 
-inter_lines = {}
-
-for line in lines:
-    for i in range(line[0], line[1]):
-        if i in inter_lines:
-            inter_lines[i] += 1
-        else:
-            inter_lines[i] = 1
-        
-
-print(max(inter_lines.values()))
+# 최댓값을 구합니다.
+max_num = max(checked)
+print(max_num)

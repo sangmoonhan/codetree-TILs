dc86364fc0239ccdae04865c4c97de057006c61d
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-07 18:01:15 +0900

    Title: 흰검 칠하기, Time: 117ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240107/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+curr = [0] * 200001
+black = [0] * 200001
+white = [0] * 200001
+w,b,g = 0,0,0
+
+start = 100000
+for _ in range(n):
+    cnt, direction = input().split()
+    cnt = int(cnt)
+
+    if direction == "R":
+        for i in range(start,start+cnt):
+            curr[i] = 1
+            black[i] += 1 
+        start += cnt
+    elif direction == "L":
+        for i in range(start-cnt, start):
+            curr[i] = 2
+            white[i] += 1
+        start -= cnt
+
+for i in range(200001):
+    if white[i] >= 2 and black[i] >= 2:
+        g += 1
+    elif curr[i] == 1:
+        b += 1
+    elif curr[i] == 2:
+        w += 1
+
+print(w,b,g)

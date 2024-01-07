e58422d389d690128ba8bb5d3bba4d1617c32a83
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-07 12:42:32 +0900

    Title: 여러가지 진수변환, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240107/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,15 @@
+n, b = tuple(map(int,input().split()))
+
+digits = []
+
+while True:
+    if n < b:
+        digits.append(n)
+        break
+
+    digits.append(n % b)
+    n //= b
+
+# print binary number
+for digit in digits[::-1]:
+    print(digit, end="")

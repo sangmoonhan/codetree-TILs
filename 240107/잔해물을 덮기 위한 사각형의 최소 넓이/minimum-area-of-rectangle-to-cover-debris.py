1aa722a4d38e47d86d47efcb92779ea028894650
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-07 19:54:39 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 161ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240107/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240107/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,28 +1,21 @@
 input_list = [list(map(int,input().split())) for _ in range(2)]
 
-
 offset = 1000
 max_number = 2001
 
-
 area_list = [[0] * max_number for _ in range(max_number)]
 
-
 x1, y1, x2, y2 = input_list[0]
-
 
 for idx in range(x1+offset, x2+offset+1):
     for idy in range(y1+offset, y2+offset+1):
         area_list[idx][idy] = 1 
 
-
 x1, y1, x2, y2 = input_list[1]
-
 
 for idx in range(x1+offset, x2+offset+1):
     for idy in range(y1+offset, y2+offset+1):
         area_list[idx][idy] = 0  
-
 
 empty_list = list()
 
@@ -36,11 +29,14 @@ for dx in range(2001):
 min_x, min_y = 2001,2001
 max_x, max_y = 0,0
 
+if empty_list == []:
+    print(0)
+else:
     
-for value in empty_list:
-    min_x ,max_x = min(min_x,value[0]), max(max_x, value[0])
-    min_y, max_y = min(min_y,value[1]), max(max_y,value[1])
-
-
-answer = (max_x - min_x) * (max_y - min_y)
-print(answer)
+    for value in empty_list:
+        min_x ,max_x = min(min_x,value[0]), max(max_x, value[0])
+        min_y, max_y = min(min_y,value[1]), max(max_y,value[1])
+    
+    answer = (max_x - min_x) * (max_y - min_y)
+    
+    print(answer)

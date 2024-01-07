df5af851e987aca6fe071c4a0e1bc67f2ee29fe7
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-07 19:40:15 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 157ms, Memory: 55MB, Status: Wrong Answer - CodeTree

---
+++ 240107/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,53 @@
+input_list = [list(map(int,input().split())) for _ in range(2)]
+
+
+offset = 1000
+max_number = 2001
+
+
+area_list = [[0] * max_number for _ in range(max_number)]
+
+
+x1, y1, x2, y2 = input_list[0]
+
+
+for idx in range(x1+offset, x2+offset+1):
+    for idy in range(y1+offset, y2+offset+1):
+        area_list[idx][idy] += 1 
+
+
+x1, y1, x2, y2 = input_list[1]
+
+
+for idx in range(x1+offset, x2+offset+1):
+    for idy in range(y1+offset, y2+offset+1):
+        area_list[idx][idy] -= 1  
+
+
+empty_list = list()
+
+# 겹치지 않는 영역들의 위치 정보를 empty_list에 저장합니다.
+
+for dx in range(2001):
+    for dy in range(2001):
+        if area_list[dx][dy] == 1:
+            empty_list.append([dx,dy])
+
+
+min_x, min_y = 2001,2001
+max_x, max_y = 0,0
+
+    
+for value in empty_list:
+    min_x ,max_x = min(min_x,value[0]), max(max_x, value[0])
+    min_y, max_y = min(min_y,value[1]), max(max_y,value[1])
+
+
+# empty_list 가 None인 특수한 경우에는 area가 0이 되게 만듭니다.
+
+if empty_list == []:
+    min_x, min_y = 0,0
+
+
+answer = (max_x - min_x) * (max_y - min_y)
+print(answer)

3c6db287115e43b142059a9e6d93de6ae9582bf1
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-07 13:14:40 +0900

    Title: 최대로 겹치는 구간, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240107/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240107/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,28 +1,18 @@
-def find_max_overlap(lines):
-    sorted_lines = sorted(lines, key=lambda x: x[0])
+n = int(input())
 
-    max_overlap_count = 0
-    current_overlap_count = 0
-    previous_end = float('-inf')
+lines = []
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    lines.append((x1, x2))
 
-    for start, end in sorted_lines:
-        if start > previous_end:
-            current_overlap_count = 1
+inter_lines = {}
+
+for line in lines:
+    for i in range(line[0], line[1]):
+        if i in inter_lines:
+            inter_lines[i] += 1
         else:
-            current_overlap_count += 1
+            inter_lines[i] = 1
+        
 
-        max_overlap_count = max(max_overlap_count, current_overlap_count)
-        previous_end = max(previous_end, end)
-
-    return max_overlap_count
-
-if __name__ == "__main__":
-    n = int(input())
-    lines = []
-
-    for _ in range(n):
-        x1, x2 = map(int, input().split())
-        lines.append((x1, x2))
-
-    result = find_max_overlap(lines)
-    print(result-1)
+print(max(inter_lines.values()))

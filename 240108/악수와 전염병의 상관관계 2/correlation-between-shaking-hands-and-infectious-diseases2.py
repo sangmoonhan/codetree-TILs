2a36f92847a8dbba9dc4e20746450b86394277f8
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-08 13:10:27 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,7 +1,7 @@
 n, k, p, T = tuple(map(int,input().split()))
 
-cnts = [0] * (n+1)
-cnts[p] = k
+cnts = [k] * (n+1)
+
 
 infected = [0] * (n+1)
 infected[p] = 1
@@ -12,7 +12,7 @@ for i in range(T):
 
     arr.append(tuple(map(int,input().split())))
 
-arr.sort()
+arr.sort(key=lambda x: x[0])
 
 for i in range(T):
 
@@ -21,14 +21,13 @@ for i in range(T):
     if infected[x] == 1 and infected[y] == 0 :
         if cnts[x] > 0 :
             infected[y] = 1
-            cnts[y] = k
+            #cnts[y] = k
             cnts[x] -= 1
     elif infected[y] == 1 and infected[x] == 0:
         if cnts[y] > 0 :
             infected[x] = 1
-            cnts[x] = k
+            #cnts[x] = k
             cnts[y] -= 1
-
 
 for i in range(1,len(infected)):
     print(infected[i],end="")

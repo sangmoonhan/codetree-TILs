d208156d39d57accc63b38a31bfd1a44a3a6bd81
sangmoonhan <153166299+sangmoonhan@users.noreply.github.com>
2024-01-10 14:20:00 +0900

    Title: 거울에 레이저 쏘기 2, Time: 185ms, Memory: 48MB, Status: Wrong Answer - CodeTree

--- 240110/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240110/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,63 +1,94 @@
 n = int(input())
 
-grid = [
-    list(input())
-    for _ in range(n)
-]
+arr = [ list(input()) for _ in range(n) ]
 
-k = int(input()) # 레이저 쏘는 위치
+# 아래 왼 오 위
+dx = [1,0,0,-1]
+dy = [0,-1,1,0]
 
-# 시작점(r, c)과 레이저 방향(curr_dir) 초기화
-r, c = -1, -1
-curr_dir = 0
+start = int(input())
 
-if k >= 1 and k <= n:
-    r, c = 0, k - 1
-    curr_dir = 0
+a = start // n
+b = start % n
 
-elif k >= n + 1 and k <= 2 * n:
-    r, c = k - n - 1, n - 1
-    curr_dir = 3
+if b == 0 :
+    a -= 1
 
-elif k >= 2 * n + 1 and k <= 3 * n:
-    r, c = n - 1, n - (k - 2 * n)
-    curr_dir = 1
+# m * 4
+if start <= n :
+    x, y, d = 0, start-1, 0 # (b-1 + n)%n
 
-else:
-    r, c = n - (k - 3 * n), 0
-    curr_dir = 2
-
-dxs, dys = [1, -1, 0, 0], [0, 0, 1, -1]
-cnt = 0
+elif start <= 2*n :
+    x, y, d = start-n-1, n-1, 1
+    
+elif start <= 3*n :
+    x, y, d = n-1, n-(start-2*n), 2
+else : 
+    x, y, d = n-(start-3*n), 0, 3
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-def simulate(curr_x, curr_y):
-    global curr_dir, cnt
+def f1(x,y,d,m):
+    
+    if m == "/" : # "/" 의 규칙
+        if d == 0 : # ↓ 아래로 오면 ← 왼쪽으로 가고
+            a, b = x + dx[1], y + dy[1]
+            c = 1
+            return a,b,c 
         
-    # 1. 방향 바꾸기
-    if grid[curr_x][curr_y] == '/':
-        if curr_dir == 0:
-            curr_dir = 3
-        elif curr_dir == 1:
-            curr_dir = 2
-        else:
-            curr_dir = 3 % curr_dir
-    else:
-        curr_dir = (curr_dir + 2) % 4
+        elif d == 1: # ← 왼쪽으로 오면 ↓ 아래로 가고
+            a, b = x + dx[0], y + dy[0]
+            c = 0
+            return a,b,c 
+
+        elif d == 2: # → 오른쪽으로 오면 ↑ 위로 가고
+            a, b = x + dx[3], y + dy[3]
+            c = 3
+            return a,b,c 
+        
+        elif d == 3: # ↑ 위로 오면 → 오른쪽으로 가고
+            a, b = x + dx[2], y + dy[2]
+            c = 2
+            return a,b,c 
+    else : # "\\" "\" 의 규칙
+        if d == 0 : # ↓ 아래로 오면 → 오른쪽으로 가고
+            a, b = x + dx[2], y + dy[2]
+            c = 2
+            return a,b,c 
+        
+        elif d == 2: # → 오른쪽으로 오면 ↓ 아래로 가고
+            a, b = x + dx[0], y + dy[0]
+            c = 0
+            return a,b,c 
+
+        elif d == 1: # ← 왼쪽으로 오면 ↑ 위로 가고
+            a, b = x + dx[3], y + dy[3]
+            c = 3
+            return a,b,c 
+        
+        elif d == 3: # ↑ 위로 오면 ← 왼쪽으로 가고
+            a, b = x + dx[1], y + dy[1]
+            c = 1
+            return a,b,c
     
-    # 2. 레이저 쏴서 이동할 다른 칸 구하기
-    nx, ny = curr_x + dxs[curr_dir], curr_y + dys[curr_dir]
+    return 2000, 2000, d
 
-    cnt += 1 # 이동 횟수 + 1
-    
-    # 3. 격자 밖으로 나간 경우 이동 종료, 아닌 경우 nx, ny를 curr_x, curr_y으로 갱신
-    if not in_range(nx, ny):
-        return
-    else:
-        simulate(nx, ny)
+#n = int(input())
+#arr = [ list(input()) for _ in range(n) ]
 
 
-simulate(r, c)
+
+brr = [x,y,d]
+
+cnt = 0
+
+while in_range(x,y):
+
+    x,y,d = list(f1(x,y,d,arr[x][y]))
+
+    cnt += 1
+
 print(cnt)
+
+#print(list(f1(brr[0],brr[1],brr[2],arr[brr[0]][brr[1]])))
